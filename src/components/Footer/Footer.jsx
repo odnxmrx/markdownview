85a8913884d351193ddcce5e479b60b4f4b59a3e
ArmandoG <armandopinedag@outlook.com>
2024-12-28 14:24:26 -0600

    refactor css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css"
 export const Footer = () => {
     return (
         <div className={styles.myFooter}>
-            <h1>my fot</h1>
+            Developed with 💙 by &#9426; <a href="https://github.com/odnxmrx" target="_blank">Armando Gama</a> - 2024
         </div>
     )
 }
