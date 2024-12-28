1265361adc8d139cce5045cf030e03f89ec7f729
ArmandoG <armandopinedag@outlook.com>
2024-12-28 15:18:18 -0600

    add button to copy markdown text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,8 @@ import styles from "./Header.module.css"
 export const Header = () => {
     return (
         <div className={styles.myHeader}>
-            <h1>header</h1>
+            <h1>Markdown formatter</h1>
+            <p>Realtime markdown previewer editor.</p>
         </div>
     )
 }
--- src/components/TextEditor/TextEditor.jsx
+++ src/components/TextEditor/TextEditor.jsx
@@ -3,16 +3,22 @@ import styles from "./TextEditor.module.css"
 // eslint-disable-next-line react/prop-types
 export const TextEditor = ({ currentInput, handleChange }) => {
 
+
+    const handleCopyText = () => {
+        navigator.clipboard.writeText(currentInput);
+    }
+
     return (
         <div className={styles.containerBox}>
             <textarea
                 name="editor"
                 id="editor"
                 cols={30}
-                rows={10}
+                rows={20}
                 value={currentInput}
                 onChange={handleChange}
             ></textarea>
+            <button type="button" className={styles.copyButton} onClick={handleCopyText}>Copy</button>
         </div >
     )
 }
